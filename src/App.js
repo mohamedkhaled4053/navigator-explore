3afdbc091fae08629d072043004cb8bedd6a1892
Mohamed Khaled <mohamedkhaled4053@gmail.com>
2023-06-25 18:59:35 +0300

    explor navigator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let obj = {
+    deviceMemory: navigator.deviceMemory,
+    connection: navigator.connection,
+    gpu: navigator.gpu,
+    hardwareConcurrency: navigator.hardwareConcurrency,
+    mediaCapabilities: navigator.mediaCapabilities,
+    storage: navigator.storage,
+    userAgent: navigator.userAgent,
+    userAgentData: navigator.userAgentData,
+    serial: navigator.serial,
+    xr: navigator.xr,
+    windowControlsOverlay: navigator.windowControlsOverlay,
+    screen:`${window.screen.width} x ${window.screen.height}`,
+  };
+
+  console.log(obj);
+
   return (
     <div className="App">
       <header className="App-header">
